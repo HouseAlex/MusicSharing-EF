4b635beec02adcbe924a771329245c79b3ca0c3a
HouseAlex <houseah@mail.uc.edu>
2024-01-12 16:09:22 -0500

    Project Setups

---
+++ MusicSharing.Business/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicSharing.Buisness.Services.Interfaces.IUserService;
+
+/// <summary>
+/// An interface for the user service.
+/// </summary>
+public interface IUserService
+{
+
+}
