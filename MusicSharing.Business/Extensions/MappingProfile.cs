4bbdfb95211103c3fcf601feb552265f1d5c152b
Alex <houseah@mail.uc.edu>
2024-01-23 18:12:48 -0500

    automapper setup

---
+++ MusicSharing.Business/Extensions/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MusicSharing.Contracts.Outputs;
+using MusicSharing.Data.Entities;
+
+namespace MusicSharing.Business.Extensions
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
