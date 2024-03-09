611ec287d377138cb18e4bba1e3e9f0361d7decb
HouseAlex <houseah@mail.uc.edu>
2024-03-09 16:23:11 -0500

    new things

--- MusicSharing.API/Controllers/PostController.cs
+++ MusicSharing.API/Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicSharing.Business.Services;
+using MusicSharing.Business.Services.Interfaces;
 
 namespace MusicSharing.API.Controllers;
 
@@ -7,6 +9,23 @@ namespace MusicSharing.API.Controllers;
 /// </summary>
 [ApiController]
 [Route("[controller]")]
-public class PostController
+public class PostController : ControllerBase
 {
+    private readonly IPostService postService;
+
+    public PostController(IPostService postService)
+    {
+        this.postService = postService;
+    }
+
+    [HttpGet("titles")]
+    public async Task<ActionResult<IEnumerable<string>>> GetPostTitles()
+    {
+        var postTitles = await postService.GetPostTitles();
+        if (postTitles == null)
+        {
+            return NotFound();
+        }
+        return Ok(postTitles);
+    }
 }
